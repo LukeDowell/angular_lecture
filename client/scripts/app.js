ae26283d8b3b8014c6046fbce2f6fbae0a2bda6e
Luke Dowell <lukedowell@gmail.com>
2015-08-13 15:10:13 -0500

    Finishes lecture

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,15 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller("WelcomeController", ['$scope', '$http', function($scope, $http) {
     $scope.heading = "Here is your message: ";
-    $scope.updateMessage = function() {
-        $scope.message = "Hello " + $scope.name + " !!!!";
+
+    $scope.getData = function() {
+      $http.get('/data').then(function(data) {
+        console.log(data);
+      });
+    };
+
+    $scope.updateMessage = function(data) {
+        console.log(data);
+        $http.post('/data', data).then($scope.getData())
     };
 }]);
--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var index = require('./routes/index');
-var path = require('path');
 var app = express();
 
+mongoose.connect('mongodb://localhost/angular_lecture');
+app.use(bodyParser.json());
 app.use('/', index);
-
 app.set("port", (process.env.PORT || 5000));
 
 app.listen(app.get("port"), function(){
